f8f16201c4189e25596aade935520e0557054147
Sang Phạm <anhsang222@gmail.com>
2020-12-08 22:19:48 +0700

    Revert "Update .depslintrc.js"

--- .depslintrc.js
+++ .depslintrc.js
@@ -9,7 +9,7 @@ module.exports = {
     '**/*-provider/**',
     '**/*.json',
   ],
-  modulePattern: [aaaaaaaa
+  modulePattern: [
     {
       pattern: /ConfigConsumer.*renderEmpty/ms,
       module: '../empty',
